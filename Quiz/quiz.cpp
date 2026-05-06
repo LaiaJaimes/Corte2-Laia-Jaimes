abda7ac175b8bc726c0619ca6142555020c242b7
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-06 11:24:56 -0500

    Creacion de clases

--- Quiz/quiz.cpp
+++ Quiz/quiz.cpp
@@ -1,11 +1,36 @@
-
 #include <iostream>
 #include <string>
 
 class product{
+
     private:
-    std::string name;
+        std::string name;
+        
+
+    protected: 
+        int price;
+        int quantity;
 
     public:
-    void setName;
+        void setName (int name){
+            this-> name = name;
+        }
+
+        void getName(){
+            std::cout ("")
+        }
+
+        void setPrice(int price) {
+            this-> product = product;
+        }
+
+        void getPrice(){
+            std::cout ()
+        }
+
+        void setQuantity (int quantity){
+            this-> quantity =  quantity;
+        }
+
+
 }
