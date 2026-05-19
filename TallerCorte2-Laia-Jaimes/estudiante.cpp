f5a1f2d82757032ed62644ab486659b4851ffd7e
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-18 19:12:58 -0500

    Incluir librerias y variables

---
+++ TallerCorte2-Laia-Jaimes/estudiante.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+Estudiante::Estudiante(
+    string nombre,
+    string apellido,
+    int edad,
+    string documento,
+    string codigo,
+    int semestre,
+    float promedio
+)
