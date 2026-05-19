f2dc45e24cc0502923eed1af19567c6e85bed1d6
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-18 21:15:28 -0500

    correccion getters y setters

---
+++ TallerCorte2-Laia-Jaimes/curso.h
@@ -0,0 +1,5 @@
+#ifndef CURSO_H
+#define CURSO_H
+
+#include "curso.h"
+
--- TallerCorte2-Laia-Jaimes/profesor.cpp
+++ TallerCorte2-Laia-Jaimes/profesor.cpp
@@ -18,13 +18,33 @@ Profesor::Profesor(
 }
 
 void Profesor::asignarCurso() {
-    cout << "Curso asignado." << endl;
-}
+    cout << "Curso asignado correctamente." << endl;
 
-void Profesor::mostrarInformacion() {
-    cout << "\n PROFESOR " << endl;
+    void Profesor::mostrarInformacion() {
+
+    cout << "\n===== PROFESOR =====" << endl;
+
     cout << "Nombre: " << nombre << endl;
     cout << "Apellido: " << apellido << endl;
+    cout << "Edad: " << edad << endl;
+    cout << "Documento: " << documento << endl;
+
     cout << "Especialidad: " << especialidad << endl;
     cout << "Salario: " << salario << endl;
+}
+string Profesor::getEspecialidad() {
+    return especialidad;
+}
+
+float Profesor::getSalario() {
+    return salario;
+}
+
+
+void Profesor::setEspecialidad(string especialidad) {
+    this->especialidad = especialidad;
+}
+
+void Profesor::setSalario(float salario) {
+    this->salario = salario;
 }
