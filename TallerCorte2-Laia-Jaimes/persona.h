6c8f57260dce84a3bb45f14aff58a4cb326e559f
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-18 19:05:16 -0500

    Creacion de clase

--- TallerCorte2-Laia-Jaimes/persona.h
+++ TallerCorte2-Laia-Jaimes/persona.h
@@ -10,4 +10,24 @@
  * @brief Clase abstracta base del sistema.
  */
 
- 
+ class Persona {
+ protected:
+    string nombre;
+    string apellido;
+    int edad;
+    string documento;
+
+ public:
+    Persona(string nombre, string apellido, int edad, string documento);
+
+    virtual void mostrarInformacion() = 0;
+
+    string getNombre();
+    void setNombre(string nombre);
+
+    virtual ~Persona();
+
+        virtual ~Persona();
+};
+
+#endif
