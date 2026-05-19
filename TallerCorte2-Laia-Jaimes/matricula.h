293c47aca5b6b1abceb879fcf0601d7626284c65
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-19 00:08:38 -0500

    private

--- TallerCorte2-Laia-Jaimes/matricula.h
+++ TallerCorte2-Laia-Jaimes/matricula.h
@@ -1,3 +1,13 @@
 #ifndef MATRICULA_H
 #define MATRICULA_H
 
+/**
+ * @class Matricula
+ * @brief Representa una matrícula universitaria.
+ */
+
+class Matricula {
+
+private:
+    string estadoMatricula;
+    float notaFinal;
