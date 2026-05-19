a68946633f12bc01dd5254a6ad77ab3ec53652db
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-18 19:09:11 -0500

    definir para estudiante

---
+++ TallerCorte2-Laia-Jaimes/estudiante.h
@@ -0,0 +1,4 @@
+#ifndef ESTUDIANTE_H
+#define ESTUDIANTE_H
+
+#include "Persona.h"
--- TallerCorte2-Laia-Jaimes/persona.cpp
+++ TallerCorte2-Laia-Jaimes/persona.cpp
@@ -14,3 +14,6 @@ string Persona::getNombre() {
 
 void Persona::setNombre(string nombre) {
     this->nombre = nombre;
+
+Persona::Persona() {
+}
