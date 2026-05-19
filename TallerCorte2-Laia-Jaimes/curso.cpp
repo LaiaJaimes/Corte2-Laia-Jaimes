8395c9c7b399b4be304a656ef31bf8eceabcfcd3
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-18 21:25:07 -0500

    setters y getters

--- TallerCorte2-Laia-Jaimes/curso.cpp
+++ TallerCorte2-Laia-Jaimes/curso.cpp
@@ -13,3 +13,38 @@ Curso::Curso(
     this->codigoCurso = codigoCurso;
     this->creditos = creditos;
 }
+void Curso::mostrarCurso() {
+
+    std::cout << "\n CURSO " << endl;
+
+    std::cout << "Nombre del curso: "
+         << nombreCurso << endl;
+
+    std::cout << "Codigo del curso: "
+         << codigoCurso << endl;
+
+    std::cout << "Creditos: "
+         << creditos << endl;
+}
+string Curso::getNombreCurso() {
+    return nombreCurso;
+}
+
+string Curso::getCodigoCurso() {
+    return codigoCurso;
+}
+
+int Curso::getCreditos() {
+    return creditos;
+}
+void Curso::setNombreCurso(string nombreCurso) {
+    this->nombreCurso = nombreCurso;
+}
+
+void Curso::setCodigoCurso(string codigoCurso) {
+    this->codigoCurso = codigoCurso;
+}
+
+void Curso::setCreditos(int creditos) {
+    this->creditos = creditos;
+}
