ea17681ccb69ce8a6a73fac629934b0bdd350588
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-18 21:31:18 -0500

    getters y setters

--- TallerCorte2-Laia-Jaimes/matricula.cpp
+++ TallerCorte2-Laia-Jaimes/matricula.cpp
@@ -9,3 +9,30 @@
  * @class Matricula
  * @brief Matricula.
  */
+
+ class Matricula {
+
+private:
+    string estadoMatricula;
+    float notaFinal;
+
+public:
+
+    Matricula(
+        string estadoMatricula,
+        float notaFinal
+    );
+
+     void mostrarMatricula();
+
+    // GETTERS
+    string getEstadoMatricula();
+    float getNotaFinal();
+
+    // SETTERS
+    void setEstadoMatricula(string estadoMatricula);
+    void setNotaFinal(float notaFinal);
+};
+
+#endif
+
