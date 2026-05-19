64ca7bb1726523f5b588e4fda637931ab47659ce
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-18 20:50:30 -0500

    variable

--- TallerCorte2-Laia-Jaimes/profesor.cpp
+++ TallerCorte2-Laia-Jaimes/profesor.cpp
@@ -3,3 +3,16 @@
 #include <cstdio>
 #include "profesor.h"
 
+Profesor::Profesor(
+    string nombre,
+    string apellido,
+    int edad,
+    string documento,
+    string especialidad,
+    float salario
+)
+: Persona(nombre, apellido, edad, documento)
+{
+    this->especialidad = especialidad;
+    this->salario = salario;
+}
