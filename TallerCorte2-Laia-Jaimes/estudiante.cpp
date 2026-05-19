62991a75b5749376a4bccb8360ee7e8b20cc16b5
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-18 19:14:10 -0500

    informacion, lista

--- TallerCorte2-Laia-Jaimes/estudiante.cpp
+++ TallerCorte2-Laia-Jaimes/estudiante.cpp
@@ -11,3 +11,23 @@ Estudiante::Estudiante(
     int semestre,
     float promedio
 )
+: Persona(nombre, apellido, edad, documento)
+{
+    this->codigo = codigo;
+    this->semestre = semestre;
+    this->promedio = promedio;
+}
+void Estudiante::inscribirCurso() {
+    cout << "Curso inscrito correctamente." << endl;
+}
+
+void Estudiante::mostrarInformacion() {
+    cout << "\n ESTUDIANTE " << endl;
+    cout << "Nombre: " << nombre << endl;
+    cout << "Apellido: " << apellido << endl;
+    cout << "Edad: " << edad << endl;
+    cout << "Documento: " << documento << endl;
+    cout << "Codigo: " << codigo << endl;
+    cout << "Semestre: " << semestre << endl;
+    cout << "Promedio: " << promedio << endl;
+}
