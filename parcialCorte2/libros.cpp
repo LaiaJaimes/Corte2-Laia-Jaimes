970e5f0fea1725065b03415129f05a96f714b620
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-20 11:03:02 -0500

    agregar librerias

---
+++ parcialCorte2/libros.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include "libros.h"
+
+
+
---
+++ parcialCorte2/libros.h
@@ -0,0 +1,15 @@
+#ifndef LIBROS_H
+#define LIBROS_H
+
+#include "libros.h"
+
+/**
+ * @class Libros 
+ * @brief regristro de libros.
+ */
+
+class libros{
+
+ }
+
+ 
