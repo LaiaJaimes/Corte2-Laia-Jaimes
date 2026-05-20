a199d6bff3cc5f8329f2af3787afcd292c106813
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-20 11:08:26 -0500

    agregar getters

--- parcialCorte2/persona.h
+++ parcialCorte2/persona.h
@@ -9,4 +9,19 @@ class Persona {
 protected:
     std::string nombre;
     std::string documento;
-}
+public:
+
+    Persona(
+        std::string nombre,
+        std::string documento
+    );
+
+    virtual void mostrarRol() = 0;
+
+    virtual void mostrarInformacion() = 0;
+
+    
+    std::string getNombre();
+
+    std::string getDocumento();
+
