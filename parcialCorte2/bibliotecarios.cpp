0cd73a75b07fa2ce4c5d95b5ef3746026ee13c15
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-20 11:46:44 -0500

    public

--- parcialCorte2/bibliotecarios.cpp
+++ parcialCorte2/bibliotecarios.cpp
@@ -8,3 +8,14 @@ class Bibliotecarios : public Persona {
 private:
     int codigoEmpleado;
     float salario;
+
+public:
+
+    Bibliotecarios(
+        std::string nombre,
+        std::string documento,
+        int codigoEmpleado,
+        float salario
+    );
+
+    
