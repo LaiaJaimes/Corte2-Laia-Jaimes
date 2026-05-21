1840b4748904aaa6409a9a7319c8cb326bbc466c
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-20 19:04:48 -0500

    libro

--- parcialCorte2-Laia-Kevin/main.cpp
+++ parcialCorte2-Laia-Kevin/main.cpp
@@ -19,4 +19,17 @@ int main() {
         501,
         3500000
     );
+    Libros libro1(
+        "El gato negro",
+        "Edgar Allan Poe",
+        200,
+        true
+    );
 
+    usuario1.mostrarRol();
+
+    usuario1.mostrarInformacion();
+
+    bibliotecario1.mostrarRol();
+
+    bibliotecario1.mostrarInformacion();
