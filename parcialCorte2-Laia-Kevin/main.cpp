d1c3731d9900230de2f5f3e91ad982dfd313433f
Laia <laia.jaimeslai@unipamplona.edu.co>
2026-05-20 19:01:23 -0500

    int

---
+++ parcialCorte2-Laia-Kevin/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "include/persona.h"
+#include "include/usuarios.h"
+#include "include/bibliotecarios.h"
+#include "include/libros.h"
+
+int main() {
+
+    Usuarios usuario1(
+        "Carlos",
+        "12345",
+        1001,
+        2
+    );
+    
